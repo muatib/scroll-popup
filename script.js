4d6dbfdf73b1ec8c46a611ecd19ed353f21bbad8
Vincent Dubois <150166653+muatib@users.noreply.github.com>
2023-12-14 14:57:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let playOnce = true;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+  } else {
+    navbar.style.height = "90px";
+  }
+
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = "1";
+    imgImprovise.style.transform = "none";
+
+    if (scrollValue > 0.85 && playOnce) {
+      popup.style.opacity = "1";
+      popup.style.transform = "none";
+      playOnce = false;
+    }
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = "0";
+  popup.style.transform = "translateX(500px";
+});
